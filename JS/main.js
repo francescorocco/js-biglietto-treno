107fcbc8c6d5f7a68d54d93e6d747138b4b26eda
Francesco Rocco <francescorocco97@gmail.com>
2023-02-22 15:38:26 +0100

    esercizio completato

--- JS/main.js
+++ JS/main.js
@@ -22,10 +22,10 @@ console.log(ticketCost);
 
 if (ageUser <= 17){
     ticketCost = ((ticketCost / 100) * 20);
-    document.getElementById(sconto).innerHTML = 'Complimenti per essere così giovane, le abbiamo applicato uno sconto del 20%';
+    document.getElementById('sconto').innerHTML = 'Complimenti per essere così giovane, le abbiamo applicato uno sconto del 20%';
 }else if (ageUser >= 65){
     ticketCost = ((ticketCost / 100) * 40);
-    document.getElementById(sconto).innerHTML = 'Complimenti per essere così grande ed avere ancora la pazienza di utilizzare i nostri servizi, le abbiamo applicato uno sconto del 40%';
+    document.getElementById('sconto').innerHTML = 'Complimenti per essere così grande ed avere ancora la pazienza di utilizzare i nostri servizi, le abbiamo applicato uno sconto del 40%';
 }
 
 console.log(ticketCost);
@@ -33,7 +33,7 @@ console.log(ticketCost);
 ticketCost = ticketCost.toFixed(2);
 console.log(ticketCost);
 
-document.getElementById(prezzo).innerHTML= `Il prezzo del biglietto è di: &{ticketCost}€`
+document.getElementById('prezzo').innerHTML= 'Il prezzo del biglietto è di: ' + ticketCost + ' € ' ;
 
 
 
